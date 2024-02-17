7add1a671607816dce9705bf2b3d3f155d147bed
Its Sasori <100895288+itsSasori@users.noreply.github.com>
2024-02-17 18:43:02 +0545

    Update settings.py

--- Campus/settings.py
+++ Campus/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'Campus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Gallary_db',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
